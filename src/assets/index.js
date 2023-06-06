29b5461a448cd38c8debd28480982ca6dc21c5bd
FazliddinFayziev <fazrez4515@gmail.com>
2023-06-05 22:31:43 -0700

    seventith commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,10 @@
 export const video = "https://res.cloudinary.com/dcrolfqsj/video/upload/v1681359507/video_thnvwb.mp4";
 export const click = "https://res.cloudinary.com/dcrolfqsj/image/upload/v1681393837/click_fl6mnv.jpg";
+export const logo = "https://res.cloudinary.com/dcrolfqsj/image/upload/v1686027128/gold-web_t3oykw.png";
 export const document = "https://res.cloudinary.com/dcrolfqsj/image/upload/v1681393837/document_aarbno.jpg"
 export const level_default = "https://res.cloudinary.com/dcrolfqsj/image/upload/v1683345934/ga1_tgkmg0.jpg";
 export const level_default_ielts = "https://res.cloudinary.com/dcrolfqsj/image/upload/v1683345945/ga2_yswtts.jpg";
 export const lessons_default = "https://res.cloudinary.com/dcrolfqsj/image/upload/v1683806651/lesson_2_utmd1b.jpg";
-export const logo = "https://res.cloudinary.com/dcrolfqsj/image/upload/v1680438370/logo-removebg-preview_fhd62w.png";
 export const login_img = "https://res.cloudinary.com/dcrolfqsj/image/upload/v1686018507/Untitled_9_nkotwl_1_b2svzf.jpg";
 export const passed = "https://res.cloudinary.com/dcrolfqsj/image/upload/v1684069289/passed-removebg-preview_ufd8ph.png";
 export const white_background_image = "https://res.cloudinary.com/dcrolfqsj/image/upload/v1686018324/bg-flag_dmfhox.jpg";
